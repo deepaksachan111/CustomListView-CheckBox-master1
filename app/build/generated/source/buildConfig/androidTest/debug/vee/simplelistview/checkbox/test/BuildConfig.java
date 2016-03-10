b6a75ab9a25f9ed69c432bc3d95d8351e5f4e005
deepak <deepaksachan8@gmail.com>
2016-03-10 15:04:07 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/vee/simplelistview/checkbox/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vee.simplelistview.checkbox.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vee.simplelistview.checkbox.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
